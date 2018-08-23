6f4a2b6b1589769985717129c31f7e7f4b03a2cb
Matthew Nakayama <mtnakayama@users.noreply.github.com>
2018-08-22 17:02:16 -0700

    Create bbtravis-build.sh

---
+++ bbtravis-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e -x
+
+conda remove -y -n test-environment --all || :
+conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION alabaster>=0.7.9 atlas cycler docopt h5py matplotlib numpy>=1.12 numpydoc>=0.5.0 pillow pygments>=2.1.0 pyqt scipy sphinx>=1.5.0 sphinx_rtd_theme>=0.1.9 six vtk
+ 
+. activate test-environment
+
+conda list
+python setup.py develop
+
+cd docs/html_docs
+make html > make.out 2>&1
+
+. deactivate
