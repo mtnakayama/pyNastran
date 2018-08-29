2f5f8ddf0d304421ed7ce8f260a1e340775bf23c
Matthew Nakayama <mtnakayama@users.noreply.github.com>
2018-08-29 16:46:17 -0700

    Update bbtravis-build.sh

--- bbtravis-build.sh
+++ bbtravis-build.sh
@@ -4,12 +4,12 @@ set -e -x
 echo ${PATH}
 echo $(ls $(echo ${PATH} | cut -d: -f1))
 
-. deactivate  # deactivate virtualenv
+# . deactivate  # deactivate virtualenv
 
 conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION || :
 conda install -q -n test-environment alabaster>=0.7.9 atlas cycler docopt h5py matplotlib numpy>=1.12 numpydoc>=0.5.0 pillow pygments>=2.1.0 pyqt scipy sphinx>=1.5.0 sphinx_rtd_theme>=0.1.9 six vtk
 
-source activate test-environment
+source /buildbot/miniconda/bin/activate test-environment
 
 conda info --envs
 conda list
